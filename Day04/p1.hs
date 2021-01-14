1c1899bda42be3a549a6d55c4cc822c1d3872afe
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2021-01-14 11:45:21 -0700

    Haskell: Day04 p1

---
+++ Day04/p1.hs
@@ -0,0 +1,27 @@
+module P1 where
+
+import Data.List.Split
+import qualified Data.Map as Map  
+
+
+main :: IO ()
+main = do
+    passports <- parseFile "passports.txt"
+    let numValid = length . filter validPassport $ passports
+    putStrLn $ "Found " ++ show numValid ++ " valid passports out of " ++ show (length passports)
+
+parseFile :: FilePath -> IO [Map.Map String String]
+parseFile filename = do 
+    contents <- readFile filename
+    let passports = splitOn "\n\n" contents
+    return (map parsePassport passports)
+
+parsePassport :: String -> Map.Map String String
+parsePassport entry = 
+    let fields = words entry
+        listFields = map (splitOn ":") fields
+        tupleFields = map (\[x, y] -> (x, y)) listFields
+    in Map.fromList tupleFields
+
+validPassport :: Map.Map String String -> Bool
+validPassport passport = foldr (\field acc -> Map.member field passport && acc) True ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
